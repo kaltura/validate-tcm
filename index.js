3bee5a99f5a9bf101267dd5dd4e41aced1c7f252
Jonathan Kanarek <jonathan.amit-kanarek@Kaltura.com>
2019-06-17 18:19:56 +0300

    compare over http

--- index.js
+++ index.js
@@ -2,50 +2,94 @@
 
 const fs = require('fs');
 const glob = require('glob');
-const couchbase = require('couchbase');
+const http = require('http');
+const https = require('https');
 const resolveEnv = require('resolve-env');
 
 const configPath = process.env.CONFIG_PATH || '/etc/kaltura/tcm/*.json';
-const couchbaseHost = process.env.COUCHBASE || 'couchbase1';
 
-const cluster = new couchbase.Cluster('couchbase://' + couchbaseHost);
-const bucket = cluster.openBucket('TCM');
+const tcm = {
+    url: process.env.TCM_URL,
+    app: process.env.TCM_APP,
+    section: process.env.TCM_SECTION,
+    appId: process.env.TCM_APP_ID,
+    appSecret: process.env.TCM_APP_SECRET
+};
+const tcmUrl = `${tcm.url}/${tcm.app}/main/${tcm.section}?app_id=${tcm.appId}&app_secret=${tcm.secret}`;
+console.log(`TCM URL [${tcmUrl}]`)
+
 
 function die(err) {
     console.error(err);
-    fs.writeFileSync('/dev/termination-log', `TCM Validation: ${err}`);
+    fs.writeFileSync('/dev/termination-log', `TCM [${tcmUrl}] Validation Failed: ${err}`);
     process.exit(-1);
 }
 
-function validateConfig(file) {
-    console.log('Reading configuration: ', file);
-    const json = resolveEnv(fs.readFileSync(file, 'utf-8'));
-    var config;
-    try{
-        config = JSON.parse(json);
+function validateConfig(tcmConfig) {
+    if(!tcmConfig || tcmConfig === 'null') {
+        return die('No Configuration');
     }
-    catch(err) {
-        return die(`Reading configuration [${file}]: err.message`);
-    }
-
-    const {app, path, regex} = config;
-    
-    bucket.get(app, function(err, result) {
+    tcmConfig = JSON.parse(tcmConfig);
+    glob(configPath, (err, files) => {
         if (err) {
-            if(err.code === 13) {
-                return die(`Application [${app}] not found`);
-            }
-            return die(`Application [${app}] couchbase error: ${err.message}`);
+            return die(err);
         }
-
-        console.dir(result);
+        files.forEach(file => {
+            console.log('Reading configuration: ', file);
+            const json = resolveEnv(fs.readFileSync(file, 'utf-8'));
+            var config;
+            try {
+                config = JSON.parse(json);
+            }
+            catch (err) {
+                return die(`Reading configuration [${file}]: ${err.message}`);
+            }
+        
+            config.forEach(({path, regex}) => {
+                var currentBranch = tcmConfig;
+                var reachedPath = '/';
+                var pathParts = path.split('/');
+                while(pathParts.length) {
+                    var pathPart = pathParts.shift();
+                    if(!currentBranch[pathPart]) {
+                        return die(`Property [${pathPart}] not found under path [${reachedPath}]`);
+                    }
+                    reachedPath += `${pathPart}/`;
+                    currentBranch = currentBranch[pathPart];
+                }
+                if(regex) {
+                    var re = new RegExp(regex);
+                    if(!re.test(currentBranch)) {
+                        return die(`Property [${path}] does not match format [${regex}]`);
+                    }
+                }
+            });
+        });
     });
 }
 
-glob(configPath, (err, files) => {
-    if(err) {
-        return die(err);
-    }
+function getConfig() {
+    return new Promise((resolve, reject) => {
+        let httpLib = tcmUrl.startsWith('https://') ? https : http;
+        httpLib.get(tcmUrl, (resp) => {
+            const { statusCode } = resp;
+            if (statusCode !== 200) {
+                resp.resume();
+                return reject(`HTTP Status Code: ${statusCode}`);
+            }
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                resolve(data);
+            });
+        }).on("error", (err) => {
+            reject(`Error: ${err.message}`);
+        });
+    });
+}
 
-    files.forEach(validateConfig);
-});
+getConfig()
+.then(validateConfig)
+.catch(die);
